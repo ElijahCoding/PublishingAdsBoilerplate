16ddc56089b7e5d50a94f2544ad848b189c3f554
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-21 18:39:24 +0300

    templating areas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    @include('layouts.partials._head')
-</head>
-<body>
-    <div id="app">
-        @include('layouts.partials._navigation')
+<html lang="en">
+    <head>
+        @include('layouts.partials._head')
+    </head>
+    <body>
+        <div id="app">
+            @include('layouts.partials._navigation')
+            <div class="container">
 
-        @yield('content')
-    </div>
+                @yield('content')
+            </div>
+        </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
+        <!-- Scripts -->
+        <script src="/js/app.js"></script>
+    </body>
 </html>
