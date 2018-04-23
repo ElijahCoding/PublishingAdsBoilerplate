fa02f52111471a18f47756be70afd464eb27303d
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-23 16:13:34 +0300

    showing listing in categories

--- resources/views/listings/partials/_base_listing.blade.php
+++ resources/views/listings/partials/_base_listing.blade.php
@@ -1,3 +1,10 @@
 <div class="media">
-  
+  <div class="media-body">
+    <h5><strong><a gref="">{{ $listing->title }}</a></strong></h5>
+
+    <ul class="list-inline">
+      <li><time>{{ $listing->created_at->diffForHumans() }}</time></li>
+      <li>{{ $listing->user->name }}</li>
+    </ul>
+  </div>
 </div>
