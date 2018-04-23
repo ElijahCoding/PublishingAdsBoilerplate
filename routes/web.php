e3332cc4eda3b5a3afee5b13b9ba2eb7012d2ed9
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-23 11:26:40 +0300

    listing categories

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/user/area/{area}', 'User\AreaController@store')->name("user.area.store");
+
+
+Route::group(['prefix' => '/{area}'], function() {
+  /**
+  * Category
+  */
+  Route::group(['prefix' => 'categories'], function() {
+    Route::get('/', 'Category\CategoryController@index')->name('categories.index');
+  });
+});
