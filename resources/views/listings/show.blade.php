0401bb543d994007c1a50e45d7a5e7ff98ceb279
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-24 01:00:57 +0300

    finishing listing page

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -3,25 +3,57 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col-md-3">
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <nav class="nav-stacked">
-              <li><a href="">Email to a friend</a></li>
-              <li><a href="">Add to favorite</a></li>
-            </nav>
+      @if (Auth::check())
+        <div class="col-md-3">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <nav class="nav-stacked">
+                <li><a href="">Email to a friend</a></li>
+                <li><a href="">Add to favorite</a></li>
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
+      @endif
 
-      <div class="col-md-9">
+      <div class="{{ Auth::check() ? 'col-md-9' : 'col-md-12' }}">
         <div class="panel panel-default">
           <div class="panel-heading">
-            {{ $listing->title }} in {{ $listing->area->name }}
+            <h4>{{ $listing->title }} in <span class="text-muted">{{ $listing->area->name }}</span></h4>
           </div>
 
           <div class="panel-body">
             {!! nl2br(e($listing->body)) !!}
+            <hr>
+            <p>Viewed x times</p>
+          </div>
+        </div>
+
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            Contact {{ $listing->user->name }}
+          </div>
+
+          <div class="panel-body">
+            @if (Auth::guest())
+              <p><a href="/register">Sign up</a> for an account or <a href="/login">sign in</a> to contact listing owners.</p>
+              @else
+            <form action="#" method="post">
+              {{ csrf_field() }}
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea name="message" rows="5" id="message" class="form-control"></textarea>
+              </div>
+
+              <div class="form-group">
+                <button type="submit" class="btn btn-default">Send</button>
+
+                <span class="help-block">
+                  This will email {{ $listing->user->name }} and they will be able to reply directly to you by email.
+                </span>
+              </div>
+            </form>
+          @endif
           </div>
         </div>
       </div>
