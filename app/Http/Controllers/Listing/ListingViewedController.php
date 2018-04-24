3e5f6d253b70de3bd1628db2f3d3e1bd607f784f
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-25 00:33:44 +0300

    Recently viewed listings

--- app/Http/Controllers/Listing/ListingViewedController.php
+++ app/Http/Controllers/Listing/ListingViewedController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 
 class ListingViewedController extends Controller
 {
+    const INDEX_LIMIT = 10;
+
     public function __construct()
     {
       return $this->middleware(['auth']);
@@ -14,6 +16,17 @@ class ListingViewedController extends Controller
 
     public function index(Request $request)
     {
+      $listings = $request->user()
+                ->viewedListings()
+                ->with(['area', 'user'])
+                ->orderByPivot('updated_at', 'desc')
+                ->isLive()
+                ->take(self::INDEX_LIMIT)
+                ->get();
 
+      return view('user.listings.viewed.index', [
+        'listings' => $listings,
+        'indexLimit' => self::INDEX_LIMIT
+      ]);
     }
 }
