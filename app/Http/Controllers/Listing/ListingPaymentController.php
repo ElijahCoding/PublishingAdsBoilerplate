53b567ef942401d1e6074a2150043f7c9d5ff118
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-26 15:48:17 +0300

    payment function

---
+++ app/Http/Controllers/Listing/ListingPaymentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Listing;
+
+use App\{Area, Listing};
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ListingPaymentController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware(['auth']);
+    }
+
+    public function show(Area $area, Listing $listing)
+    {
+      if ($listing->user_id === auth()->user()->id) {
+        return view('listings.payment.show', compact('listing'));
+      }
+
+    }
+
+    public function store(Request $request, Area $area, Listing $listing)
+    {
+      if ($listing->user_id === auth()->user()->id) {
+        if($listing->live()) {
+          return back();
+        }
+      }
+    }
+}
